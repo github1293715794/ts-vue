3c3e539c28f90310753ad6ad63faba74e3252f1c
github1293715794 <1293715794@qq.com>
2019-05-27 22:23:40 +0800

    xhr

---
+++ xhr.js
@@ -0,0 +1,8 @@
+{
+  let xhr = new XMLHttpRequest();
+  xhr.open('POST', 'http://i.baidu.com/calendars/calendars/listInfo', false);
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded'); 
+  xhr.send('fromdt=15*****589&todt=15*****0');
+  // form data 
+}
+
