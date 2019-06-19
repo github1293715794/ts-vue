fc29807a0097451e19f5d2d5d9cb3fbe527ab19c
github1293715794 <1293715794@qq.com>
2019-06-20 00:19:57 +0800

    ES6

--- ES6/0619循环.js
+++ ES6/0619循环.js
@@ -5,8 +5,28 @@
  */
 
 // 循环
-let arr = [1,3,4,2]
-arr.forEach()
-arr.map()
+let arr = ['apple', 'banana', 'pear']
+arr.forEach((val, index, arr) => {
+  console.log(val, index, arr)
+})
+// 用箭头函数 第二个参数改this无效 都是window  当前定义所在的对象
+
+// 改造数组  映射
+// 正常情况下需要配合return 返回是一个新的数组
+// 若是没有return 相当于forEach
+{
+  let arr1 = [
+    {aa: '111', bb: '222', cc: '333'},
+    {aa: '4', bb: '222', cc: '333'},
+    {aa: '5', bb: '222', cc: '333'},
+  ]
+  arr1.map((val, index, arr) => {
+    console.log(val, index, arr)
+  })
+  let newArr = arr1.map((val, index, arr) => {
+    return 1
+  })
+  console.log(newArr)
+}
 arr.filter()
 
