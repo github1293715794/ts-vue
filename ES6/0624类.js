b47653da17a0aab04a47294d2354734a1e19683f
github1293715794 <1293715794@qq.com>
2019-06-24 22:34:53 +0800

    ES6

--- ES6/0624类.js
+++ ES6/0624类.js
@@ -18,3 +18,24 @@ class Person {
 }
 
 let p1 = new Person('js', 12);
+
+// 类  方法名是变量
+{
+  let aaa = 'strive';
+  class Person {
+    constructor(name, age) {
+      // 构造方法 调用new时，自动执行
+      console.log(name, age);
+      this.name = name;
+      this.age = age;
+    }
+    showName() {
+      return `名字为${this.name}`;
+    }
+    [aaa]() {
+      return '方法名是个变量'
+    }
+  }
+  let p1 = new Person('js', 12);
+  console.log(p1[aaa]());
+}
